e924ca2835258ffdb295009d2d3cbfedcd4f93aa
tamura <thi@tamuranoMacBook-Pro.local>
2021-04-18 22:33:04 +0900

    fix

--- ch_10.R
+++ ch_10.R
@@ -21,17 +21,17 @@ data.ssp2015 = list(
 #[プロセスA]
 fit_A <- stan(file='~/BMS/ch10/model/model-10-1.stan',pars=c('m','s','p','b','log_lik'),
              data=data.ssp2015,seed=1234,warmup=500, iter=1500,chains = 4)
-summary(fit_A)$summary[c('m','s','p','b'),c(1,4,8,9,10)]
 mcmc_combo(fit_A, pars = c('m','s','p','b'))
+summary(fit_A)$summary[c('m','s','p','b'),c(1,4,8,9,10)]
 
 #[プロセスB]投資に失敗しても年収は下がらないモデル
 fit_B <- stan(file="./stan/model_B.stan",pars=c('m','s','p','b'),
              data=data.ssp2015,seed=1234,warmup=500, iter=1500,chains = 4)
-summary(fit_B)$summary[c('m','s','p','b'),c(1,4,8,9,10)]
 mcmc_combo(fit_B, pars = c('m','s','p','b'))
+summary(fit_B)$summary[c('m','s','p','b'),c(1,4,8,9,10)]
 
 #[プロセスC]成功確率pに個人差があり、それは性別と教育年数で説明できる
 fitC <- stan(file="./stan/model_C.stan",
              data=data.ssp2015,seed=1234,warmup=500, iter=1500,chains = 4)
-summary(fitC)$summary[c('m[1]','s[1]','a[1]','a[2]','b'),c(1,4,8,9,10)]
 mcmc_combo(fitC, pars = c('m[1]','s[1]','a[1]','a[2]','b'))
+summary(fitC)$summary[c('m[1]','s[1]','a[1]','a[2]','b'),c(1,4,8,9,10)]
