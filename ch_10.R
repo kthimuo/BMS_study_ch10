3b33d4e8ec16b80759ac3eeec7deedef42ba0271
tamura <thi@tamuranoMacBook-Pro.local>
2021-04-18 22:22:11 +0900

    fix

--- ch_10.R
+++ ch_10.R
@@ -32,7 +32,7 @@ summary(fit1)$summary[c('m','s','p','b'),c(1,4,8,9,10)]
 mcmc_combo(fit1, pars = c('m','s','p','b'))
 
 #[アレンジ2]成功確率pに個人差があり、それは性別と教育年数で説明できる
-fit2 <- stan(file="./stan/model_2.stan",pars=c('m','s','a','b'),
-             data=data.ssp2015,seed=1,warmup=500, iter=1500,chains = 4)
+fit2 <- stan(file="./stan/model_2.stan",
+             data=data.ssp2015,seed=1234,warmup=500, iter=1500,chains = 4)
 summary(fit2)$summary[c('m[1]','s[1]','a[1]','a[2]','b'),c(1,4,8,9,10)]
 mcmc_combo(fit2, pars = c('m[1]','s[1]','a[1]','b'))
