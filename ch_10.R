3ed1079e2b8145ea462c7f0561416599f8e72223
tamura <thi@tamuranoMacBook-Pro.local>
2021-04-18 22:23:23 +0900

    fix

--- ch_10.R
+++ ch_10.R
@@ -5,11 +5,9 @@ options(mc.cores=parallel::detectCores())
 options(mc.cores=1)
 
 data <- read.csv('~/BMS/ch08/SSPI2015.csv')
-
+data <- data[data$Y>0 & data$Y<2500,]
 #年収が0または2500万以上は除外
-data %>%
-  filter(Y <= 2500) %>%
-  filter(Y >0) ->data
+
 #年収を対数化する
 data$Y <- log(data$Y)
 data.ssp2015 = list(
@@ -20,19 +18,20 @@ data.ssp2015 = list(
   AGE = data$AGE
 )
 
-#本
-fit_normal <- stan(file='~/BMS/ch10/model/model-10-1.stan',pars=c('m','s','p','b','log_lik'),
-             data=data.ssp2015,seed=1,warmup=500, iter=1500,chains = 4)
-summary(fit_normal)$summary[c('m','s','p','b'),c(1,4,8,9,10)]
-
-#[アレンジ1]投資に失敗しても年収は下がらないモデル
-fit1 <- stan(file="./stan/model_1.stan",pars=c('m','s','p','b'),
-             data=data.ssp2015,seed=1,warmup=500, iter=1500,chains = 4)
-summary(fit1)$summary[c('m','s','p','b'),c(1,4,8,9,10)]
-mcmc_combo(fit1, pars = c('m','s','p','b'))
-
-#[アレンジ2]成功確率pに個人差があり、それは性別と教育年数で説明できる
-fit2 <- stan(file="./stan/model_2.stan",
+#[プロセスA]
+fit_A <- stan(file='~/BMS/ch10/model/model-10-1.stan',pars=c('m','s','p','b','log_lik'),
              data=data.ssp2015,seed=1234,warmup=500, iter=1500,chains = 4)
-summary(fit2)$summary[c('m[1]','s[1]','a[1]','a[2]','b'),c(1,4,8,9,10)]
-mcmc_combo(fit2, pars = c('m[1]','s[1]','a[1]','b'))
+summary(fit_A)$summary[c('m','s','p','b'),c(1,4,8,9,10)]
+mcmc_combo(fit_A, pars = c('m','s','p','b'))
+
+#[プロセスB]投資に失敗しても年収は下がらないモデル
+fit_B <- stan(file="./stan/model_B.stan",pars=c('m','s','p','b'),
+             data=data.ssp2015,seed=1234,warmup=500, iter=1500,chains = 4)
+summary(fit_B)$summary[c('m','s','p','b'),c(1,4,8,9,10)]
+mcmc_combo(fit_B, pars = c('m','s','p','b'))
+
+#[プロセスC]成功確率pに個人差があり、それは性別と教育年数で説明できる
+fitC <- stan(file="./stan/model_C.stan",
+             data=data.ssp2015,seed=1234,warmup=500, iter=1500,chains = 4)
+summary(fitC)$summary[c('m[1]','s[1]','a[1]','a[2]','b'),c(1,4,8,9,10)]
+mcmc_combo(fitC, pars = c('m[1]','s[1]','a[1]','a[2]','b'))
